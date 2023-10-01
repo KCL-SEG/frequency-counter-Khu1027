4fe6c18cd63888983cd6bf64010c1d0762dc05c3
Khulood <khulood1027@gmail.com>
2023-10-01 20:14:45 +0100

    Complete Exercice 1.2

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,11 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    for item in items:
+        key = str(item)
+        #checks if the key exists
+        if frequencies.get(key) == None:
+            frequencies.update({key: 1})
+        else:
+            frequencies.update({key: frequencies.get(key)+1})
     return frequencies
